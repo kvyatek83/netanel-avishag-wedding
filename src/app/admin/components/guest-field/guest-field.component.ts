7194e4e3eaa6af6bc54ff8ce9400df379a1dc2fb
Netanel Kvyatek <kvyatek83@gmail.com>
2023-07-21 14:09:00 +0300

    guest-field phone format

--- src/app/admin/components/guest-field/guest-field.component.ts
+++ src/app/admin/components/guest-field/guest-field.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-type ControlType = 'string' | 'boolean';
+type ControlType = 'string' | 'boolean' | 'phone';
 type ThemeType = 'fill' | 'outline';
 @Component({
   selector: 'app-guest-field',
@@ -11,17 +11,21 @@ type ThemeType = 'fill' | 'outline';
 export class GuestFieldComponent implements OnChanges {
   @Input() externalFormControl!: FormControl;
   @Input() label!: string;
+  @Input() isRtl = false;
 
   controlType: ControlType | undefined;
   theme: ThemeType = 'outline';
 
-  constructor() {}
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['externalFormControl']?.currentValue) {
       this.controlType =
         this.externalFormControl?.value === true ||
         this.externalFormControl?.value == false
           ? 'boolean'
+          : this.externalFormControl?.value.startsWith('+972') ||
+            this.externalFormControl?.value.startsWith('05')
+          ? 'phone'
           : 'string';
     }
   }
